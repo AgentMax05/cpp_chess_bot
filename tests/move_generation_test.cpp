d0dc452707c869e20e87fdf44b25c0f3bcd207a4
AgentMax05 <maximvek05@gmail.com>
2021-05-22 15:26:22 -0400

    added new test to compare num of moves to proven num of moves

--- tests/move_generation_test.cpp
+++ tests/move_generation_test.cpp
@@ -2,6 +2,9 @@
 #include "board.h"
 #include "move_generation.h"
 #include "eval.h"
+#include "check_legal.h"
+
+#include <vector>
 
 // Demonstrate some basic assertions.
 TEST(MoveGenerationTest, CountGeneratedMoves) {
@@ -15,3 +18,59 @@ TEST(MoveGenerationTest, CountGeneratedMoves) {
 }
 
 
+int possible_moves(Board board, bool isWhite, int depth) {
+  vector<Move> moves = generate_moves(board, isWhite);
+  filter_moves(board, moves, isWhite);
+
+  if (depth == 0) {
+
+    int legal_moves = 0;
+
+    for (int i = 0; i < moves.size(); i++) {
+      board.make_move(moves[i].piece, moves[i].move, isWhite);
+      set_attacking(board);
+
+      if (!king_check(board, isWhite)) {
+        legal_moves++;
+      }
+      // undo the move here
+      board.make_move(moves[i].piece, moves[i].move, isWhite);
+    }
+
+    return moves.size();
+  }
+
+  int found_moves = 0;
+
+  for (int i = 0; i < moves.size(); i++) {
+    board.make_move(moves[i].piece, moves[i].move, isWhite);
+    set_attacking(board);
+
+    if (!king_check(board, isWhite)) {
+      found_moves += possible_moves(board, isWhite == false, depth - 1);
+    }
+    // undo made move
+    board.make_move(moves[i].piece, moves[i].move, isWhite);
+  }
+
+  return found_moves;
+
+}
+
+
+// Compare number of generated moves against proven number at each depth
+TEST(MoveGenerationTest, CompareGeneratedMoves) {
+
+  // proven number of moves at depth index (depth 0 is really depth 1, or 1 move for white)
+  // proven move counts taken from https://en.wikipedia.org/wiki/Shannon_number
+  vector<int> proven_number = {20, 400, 8902, 197281, 4865609, 119060324, 3195901860}
+
+  Board board;
+  board.init_board(BoardConstants::DefaultFEN);
+  board.update_boards();
+  set_attacking(board);
+
+  for (int i = 0; i < 4; i++) {
+    EXPECT_EQ(possible_moves(board, true, i), proven_number[i]);
+  }
+}
